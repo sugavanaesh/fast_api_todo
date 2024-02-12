58de467f1a40ae5f6f1f27dfaae69517a97471c9
sugavanaesh <sugisubbu123@gmail.com>
2024-02-12 14:36:25 +0530

    CRUD

--- model/model.py
+++ model/model.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class Model(BaseModel):
     text : str 
     is_done : bool = False
+
+class UpdateModel(BaseModel):
+    id:int
+    new_text:str=""
+    is_complete:bool = False
--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
-from db.connection import connection,engine,session
+from db.connection import connection, engine, session
 from db.table import Todo
-from model.model import Model
+from model.model import Model, UpdateModel
 
 app = FastAPI()
 connection()
@@ -11,15 +11,17 @@ Todo.metadata.create_all(engine)
 
 @app.get("/")
 def home():
-    return{"msg":"I'M FAST"}
+    return {"msg": "I'M FAST"}
+
 
 @app.post("/insertTodo")
-async def insert(payload : Model):
+async def insert(payload: Model):
     data = Todo(**payload.model_dump())
     session.add(data)
     session.commit()
     # session.close()
-    return {"Todo added":data.text}
+    return {"Todo added": data.text}
+
 
 @app.get("/all")
 async def get_all():
@@ -28,18 +30,26 @@ async def get_all():
 
 
 @app.put("/update/{id}")
-async def update(
-    id:int,
-    new_text:str="",
-    is_complete:bool = False
-):
-    todo_query = session.query(Todo).filter(Todo.id==id)
+async def update(payload: UpdateModel):
+    todo_query = session.query(Todo).filter(Todo.id == payload.id)
     res = todo_query.first()
-    if new_text:
-        res.text = new_text
-    res.is_done = is_complete
+    if payload.new_text:
+        res.text = payload.new_text
+    if payload.is_complete:
+        res.is_done = True
+    res.is_done = payload.is_complete
     session.add(res)
     session.commit()
 
+
+@app.delete("/{id}")
+async def delete(id):
+    todo_query = session.query(Todo).filter(Todo.id == id).first()
+    print(todo_query)
+    session.delete(todo_query)
+    session.commit()
+
+
+
 if __name__ == "__main__":
-    uvicorn.run("server:app",reload=True)
+    uvicorn.run("server:app", reload=True)
